0e313a52cbc751338a871086cfed8d4dbb60ef5a
lucas Guilbert <lucas.guilbert15@gmail.com>
2018-02-07 19:35:41 +0100

    maj

---
+++ src/Core/ClientConnexion.java
@@ -0,0 +1,12 @@
+package Core;
+
+import java.net.Socket;
+
+public class ClientConnexion implements Runnable {
+	private Socket con = null;
+	
+	@Override
+	public void run() {
+		
+	}
+}
--- src/Core/Fenetre.java
+++ src/Core/Fenetre.java
@@ -1,5 +1,9 @@
 package Core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -12,6 +16,7 @@ public class Fenetre extends JFrame {
 	private JPanel container = new JPanel();
 	private FileTree client_tree;
 	private JScrollPane client_tree_view;
+	private JButton btn_test;
 	
 	public Fenetre(int w, int h, String t) {
 		this.setTitle(t);
@@ -20,6 +25,12 @@ public class Fenetre extends JFrame {
 		this.setLocationRelativeTo(null);
 		
 		this.initClientTree();
+		btn_test = new JButton("Envoyé bonjour");
+		btn_test.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		
 		this.setContentPane(container);
 		this.setVisible(true);
